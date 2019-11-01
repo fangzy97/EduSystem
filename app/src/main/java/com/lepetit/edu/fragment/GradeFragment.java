189b76eeb0e5f458e09aa6d586bde5cc75445d8c
includeYY <1299012718@qq.com>
2019-11-01 18:36:36 +0800

    添加成绩的fragment

---
+++ app/src/main/java/com/lepetit/edu/fragment/GradeFragment.java
@@ -0,0 +1,39 @@
+package com.lepetit.edu.fragment;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lepetit.edu.R;
+import com.lepetit.edu.view_model.GradeViewModel;
+
+public class GradeFragment extends Fragment {
+
+    private GradeViewModel mViewModel;
+
+    public static GradeFragment newInstance() {
+        return new GradeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.grade_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(GradeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/lepetit/edu/view_model/GradeViewModel.java
@@ -0,0 +1,7 @@
+package com.lepetit.edu.view_model;
+
+import androidx.lifecycle.ViewModel;
+
+public class GradeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

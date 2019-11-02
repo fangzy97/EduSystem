ae6273350adff726d59dcbd545a416d536334e0f
includeYY <1299012718@qq.com>
2019-11-02 18:01:54 +0800

    引入LitePal

--- app/src/main/java/com/lepetit/edu/application/MyApplication.java
+++ app/src/main/java/com/lepetit/edu/application/MyApplication.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
 
+import org.litepal.LitePal;
+
 public class MyApplication extends Application {
     @SuppressLint("StaticFieldLeak")
     private static Context context;
@@ -15,6 +17,7 @@ public class MyApplication extends Application {
         super.onCreate();
         context = getApplicationContext();
         loginStatus = false;
+        LitePal.initialize(context);
     }
 
     public static Context getContext() {

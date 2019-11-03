504a67adb2a9fc937037cdb9cacb258c25070ef9
includeYY <1299012718@qq.com>
2019-11-03 10:50:19 +0800

    修改课程表的接口

--- app/src/main/java/com/lepetit/edu/inter/IClassSchedule.java
+++ app/src/main/java/com/lepetit/edu/inter/IClassSchedule.java
@@ -1,19 +1,23 @@
 package com.lepetit.edu.inter;
 
-import com.lepetit.edu.callback.GetTermSpinnerAdapterCallback;
+import android.widget.ArrayAdapter;
+
+import com.lepetit.edu.callback.SetTermSpinnerCallback;
 
 public interface IClassSchedule {
-    /*
-    * 设置学期的下拉框内容
-    */
-    void setTermSpinner(GetTermSpinnerAdapterCallback callback);
+    /**
+     * 设置学期的下拉框内容
+     * @param callback 回调函数用以更新界面
+     * */
+    void setTermSpinner(SetTermSpinnerCallback callback);
 
-    /*
-    * 设置默认选择的周数
-    */
-    void setWeekSpinner();
+    /**
+     * 设置周数下拉框默认显示的内容（即当前周数）
+     * @return 当前周数的index
+     * */
+    int setWeekSpinner();
 
-    /*
+    /**
     * 将课程表显示到屏幕上
     */
     void displayClassSchedule();

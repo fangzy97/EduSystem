86946094b81ea8776c838fe5545818f90cfdc82c
includeYY <1299012718@qq.com>
2019-11-08 17:19:35 +0800

    增加计算周数的方法

--- app/src/main/java/com/lepetit/edu/util/DateUtil.java
+++ app/src/main/java/com/lepetit/edu/util/DateUtil.java
@@ -2,9 +2,15 @@ package com.lepetit.edu.util;
 
 import com.lepetit.edu.callback.GetDateCallback;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /*
 * 该类用于处理各种与时间有关的操作
@@ -52,12 +58,41 @@ public class DateUtil {
         return termList;
     }
 
+    /**
+     * 获取当前学期
+     * @return 当前学期格式化之后的字符串
+     * */
     public String getCurrentTerm() {
         if (month > 1 && month < 8) {
             return (year - 1) + "-" + year + "-2";
         } else {
             return year + "-" + (year + 1) + "-1";
         }
+    }
+
+    /**
+     * 计算今天是第几个教学周
+     * @param firstDay 开学第一天，一般为周五，不计算在教学周中
+     * @param lastDay 教学周最后一天
+     * @return 周数*/
+    public int getPastWeek(@NotNull String firstDay, @NotNull String lastDay) throws ParseException {
+        // 将字符串转换为Date类型
+        String today = year + "-" + month + "-" + day;
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+        Date mFirstDay = format.parse(firstDay);
+        Date mToday = format.parse(today);
+        Date mLastDay = format.parse(lastDay);
+        // 计算今天和开学第一天相差的时间
+        long pastTime;
+        long threeDay = 1000 * 3600 * 24 * 3;
+        if (mToday.before(mFirstDay)) {
+            pastTime = 0;
+        } else if (mToday.after(mLastDay)) {
+            pastTime = mLastDay.getTime() - mFirstDay.getTime() - threeDay;
+        } else {
+            pastTime = mToday.getTime() - mFirstDay.getTime() - threeDay;
+        }
+        return (int)(pastTime / (1000 * 3600 * 24 * 7) + 1);
     }
 
     /*

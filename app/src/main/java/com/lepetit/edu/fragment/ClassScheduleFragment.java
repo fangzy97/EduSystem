43b725a1581fbd82393e32c56e7cc776eda8a018
includeYY <1299012718@qq.com>
2019-11-08 17:17:50 +0800

    增加一个显示当前数据为最新数据还是本地数据的显示框

--- app/src/main/java/com/lepetit/edu/fragment/ClassScheduleFragment.java
+++ app/src/main/java/com/lepetit/edu/fragment/ClassScheduleFragment.java
@@ -6,10 +6,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.lepetit.edu.R;
+import com.lepetit.edu.activity.MainActivity;
+import com.lepetit.edu.application.MyApplication;
+import com.lepetit.edu.callback.SetWeekSpinnerCallback;
 import com.lepetit.edu.controller.ClassScheduleController;
 import com.lepetit.edu.inter.IClassSchedule;
+
+import java.text.ParseException;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,13 +34,17 @@ public class ClassScheduleFragment extends Fragment {
     public GridLayout gridLayout;
     @BindView(R.id.class_schedule_swipe)
     SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.class_schedule_status)
+    public TextView statusText;
 
     private IClassSchedule classSchedule = new ClassScheduleController();
+    private MainActivity activity;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.class_schedule_fragment, container, false);
         ButterKnife.bind(this, root);
+        activity = (MainActivity) getActivity();
         setTermSpinner();
         setWeekSpinner();
         return root;
@@ -46,15 +56,33 @@ public class ClassScheduleFragment extends Fragment {
     private void setTermSpinner() {
         classSchedule.setTermSpinner((adapter, index) -> {
             termSpinner.setAdapter(adapter);
-            termSpinner.setSelection(index);
+            termSpinner.setSelection(index, true);
         });
     }
 
-    /*
-    * 设置当前周
-    */
+    /**
+     * 设置周数下拉框默认显示的内容
+     * */
     private void setWeekSpinner() {
-        int index = classSchedule.setWeekSpinner();
-        weekSpinner.setSelection(index);
+        try {
+            classSchedule.setWeekSpinner(new SetWeekSpinnerCallback() {
+                @Override
+                public void onSuccess(int index, int status) {
+                    weekSpinner.setSelection(index, true);
+                    if (status == MyApplication.ONLINE) {
+                        statusText.setText("最新");
+                    } else if (status == MyApplication.LOCAL) {
+                        statusText.setText("本地");
+                    }
+                }
+
+                @Override
+                public void onFailed() {
+                    activity.displayToast("请连接到校园网后下拉刷新！");
+                }
+            });
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }

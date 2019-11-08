9828094c3e31b7b0690bbd0a0dcea1cea1be1be5
includeYY <1299012718@qq.com>
2019-11-08 17:21:31 +0800

    去掉空指针检测

--- app/src/main/java/com/lepetit/edu/inter/ILogin.java
+++ app/src/main/java/com/lepetit/edu/inter/ILogin.java
@@ -2,8 +2,6 @@ package com.lepetit.edu.inter;
 
 import com.lepetit.edu.callback.LoginCallback;
 
-import org.jetbrains.annotations.NotNull;
-
 public interface ILogin {
-    void startLogin(@NotNull String username, @NotNull String password, @NotNull LoginCallback callback);
+    void startLogin(String username, String password, LoginCallback callback);
 }
